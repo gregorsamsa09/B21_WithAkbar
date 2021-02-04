4003e76c245c27df7d53dd2d55d51b52f36f3735
mehmet alkan <mehmetalkann@gmail.com>
2021-02-04 20:51:38 +0100

    b21 is dedicated

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("hi from git , last day");
+        // b21 is excited
+        // we are decicated
     }
 }
